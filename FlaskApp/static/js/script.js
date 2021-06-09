6a333f2345de4667f3f7e089e4c6505b59286b14
mssreerag <sreeragms@cet.ac.in>
2021-06-09 20:50:07 +0530

    backup commit

---
+++ FlaskApp/static/js/script.js
@@ -0,0 +1,16 @@
+var somkingStatus = document.getElementById('smokingStatus');
+console.log("Start0")
+var sexStatus = document.getElementById('sex');
+
+function smokeDropDown(a){
+    console.log("Start1")
+
+    somkingStatus.innerHTML=a;
+
+}
+function sexDropDown(a){
+    console.log("Start1")
+
+    sexStatus.innerHTML=a;
+
+}
